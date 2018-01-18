e5ad80d5189815c98c29d81eb28c84ef7692ef31
Greg King <mrgregking@msn.com>
2018-01-18 00:33:40 -0800

    Added UserController Methods

--- app/User.php
+++ app/User.php
@@ -5,9 +5,18 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
 class User extends Authenticatable
 {
     use Notifiable;
+   
+    const VERIFIED_USER = '1';
+    const UNVERIFIED_USER = '0';
+
+    const ADMIN_USER = 'true';
+    const REGULAR_USER = 'false';
+
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +24,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
     ];
 
     /**
@@ -24,6 +38,22 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token',
     ];
+    public function  isVerified()
+        {
+            return $this.verified == User::VERIFIED_USER;
+        }
+
+    public function  isAdmin()
+        {
+            return $this.admin == User::ADMIN_USER;
+        }
+
+    public static function generateVerificationCode()
+        {
+            return str_random(40);
+        }
 }
